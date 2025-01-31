0b8691491a50414982ffdef7a2f6b8bdddf7cbaa
teto-soft <48591301+teto-soft@users.noreply.github.com>
2025-01-31 22:29:42 +0900

    update: make functional

--- src/domain/task/task.ts
+++ src/domain/task/task.ts
@@ -1,48 +1,53 @@
-import { z } from "zod";
-import { ulid } from "../../libs/ulid";
+import { Result } from "neverthrow";
+import { createTitle, InvalidTitleError, Title } from "../value-objects/title";
+import { createId, Id, InvalidIdError } from "../value-objects/id";
+import { createIsDone, IsDone } from "../value-objects/isDone";
 
-export class Task {
-  readonly #id: string;
-  #title: string;
-  #isDone: boolean;
+type ITask = Readonly<{
+  id: Id;
+  title: Title;
+  isDone: IsDone;
+}>;
 
-  private readonly titleSchema = z
-    .string()
-    .min(1, "title must not be empty")
-    .max(100, "title must be less than 100 characters");
+type CreateTaskProps = Readonly<{
+  title: string;
+  isDone: boolean;
+}>;
 
-  public constructor(
-    props: { title: string } | { id: string; title: string; done: boolean },
-  ) {
-    const fromData = "id" in props;
-    if (fromData) {
-      this.#id = props.id;
-      this.#title = props.title;
-      this.#isDone = props.done;
-    } else {
-      this.#id = ulid();
-      this.#title = this.titleSchema.parse(props.title);
-      this.#isDone = false;
-    }
-  }
+type ReconstructTaskProps = Readonly<{
+  id: string;
+  title: string;
+  isDone: boolean;
+}>;
 
-  public get id() {
-    return this.#id;
-  }
+type TaskError = InvalidIdError | InvalidTitleError;
 
-  public get title() {
-    return this.#title;
-  }
-
-  public get isDone() {
-    return this.#isDone;
-  }
-
-  public edit(title: string) {
-    this.#title = this.titleSchema.parse(title);
-  }
-
-  public makeAsDone() {
-    this.#isDone = true;
-  }
-}
+export const Task = {
+  create: (props: CreateTaskProps): Result<ITask, TaskError> => {
+    return createId().andThen((id) =>
+      createTitle(props.title).map((title) => ({
+        id,
+        title,
+        isDone: createIsDone(props.isDone),
+      })),
+    );
+  },
+  reconstruct: (props: ReconstructTaskProps): Result<ITask, TaskError> => {
+    return createId(props.id).andThen((id) =>
+      createTitle(props.title).map((title) => ({
+        id,
+        title,
+        isDone: createIsDone(props.isDone),
+      })),
+    );
+  },
+  toggleDone: (task: ITask): ITask => ({
+    ...task,
+    isDone: createIsDone(!task.isDone),
+  }),
+  updateTitle: (task: ITask, newTitle: string): Result<ITask, TaskError> =>
+    createTitle(newTitle).map((title) => ({
+      ...task,
+      title,
+    })),
+};
