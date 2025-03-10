e6856dcb3747652ce6f0425d970cc05ed0e396ec
teto-soft <48591301+teto-soft@users.noreply.github.com>
2025-03-10 22:42:58 +0900

    add: drizzle schema

--- src/libs/drizzle/schema.ts
+++ src/libs/drizzle/schema.ts
@@ -1,7 +1,33 @@
-import {pgTable, varchar} from "drizzle-orm/pg-core";
+import {pgTable, unique, varchar} from "drizzle-orm/pg-core";
 
 export const tasks = pgTable("tasks", {
-  id: varchar("id").notNull(),
+  id: varchar("id").primaryKey(),
   title: varchar("title").notNull(),
   body: varchar("body").notNull(),
 });
+
+export const teams = pgTable("teams", {
+  id: varchar("id").primaryKey(),
+  name: varchar("name").notNull(),
+});
+
+export const participants = pgTable("participants", {
+  id: varchar("id").primaryKey(),
+  teamId: varchar("team_id").notNull(),
+  name: varchar("name").notNull(),
+  email: varchar("email").notNull(),
+  enrollmentStatus: varchar("enrollment_status").notNull(),
+});
+
+export const assignments = pgTable(
+  "assignments",
+  {
+    id: varchar("id").primaryKey(),
+    taskId: varchar("task_id").notNull(),
+    participantId: varchar("participant_id").notNull(),
+    progressStatus: varchar("progress_status").notNull(),
+  },
+  ({ taskId, participantId }) => [
+    unique("task_participant_idx").on(taskId, participantId),
+  ],
+);
